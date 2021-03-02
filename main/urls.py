a71e354188dc06a8f22481ebf56b4736a11bf3da
Syed Osama <osamasyed270@gmail.com>
2021-03-02 22:01:51 +0500

    4 commit

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from main.views import home
+from main.views import home, howToRegister, gameRules, contactUs, registerNow
 
 urlpatterns = [
     path('', home, name='home'),
+    path('how-to-register', howToRegister, name='how-to-register'),
+    path('game-rules', gameRules, name='game-rules'),
+    path('contact-us', contactUs, name='contact-us'),
+    path('register-now', registerNow, name='register-now'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render, HttpResponse, redirect
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+def howToRegister(request):
+    return render(request, 'main/how-to-register.html')
+
+def gameRules(request):
+    return render(request, 'main/game-rules.html')
+
+def contactUs(request):
+    return render(request, 'main/contact-us.html')
+
+def registerNow(request):
+    return render(request, 'main/register-now.html')
